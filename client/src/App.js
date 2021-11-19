7f30459dc406f835731f30d03580ac106c8a4427
qqbit <pavlik.merkulov@gmail.com>
2021-11-19 13:15:07 +0300

    design for avito

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,32 @@
-import './App.css';
-import { BrowserRouter as Router, Redirect, Route } from 'react-router-dom'
-import { MyPlacemark } from './components/MapTest2/MapTest2';
-import Login from './components/auth/Login';
-import SignUp from './components/auth/Signup';
-import Logout from './components/auth/Logout';
+import "./App.css";
+import { BrowserRouter as Router, Redirect, Route } from "react-router-dom";
+import { MyPlacemark } from "./components/MapTest2/MapTest2";
+import Login from "./components/auth/Login";
+import SignUp from "./components/auth/Signup";
+import Logout from "./components/auth/Logout";
 
-// import MapBasics from './components/MapTest2/MapTest2';
-// import PlacemarkDemo from './components/MapTest2/MapTest2';
+// import MyPlacemark from "./components/MapTest2/MapTest2";
+import PlacemarkDemo from "./components/MapTest2/MapTest2";
 
 import HomePage from "./components/HomePage/HomePage";
 import Prodavito from "./components/Prodavito/Prodavito";
 import Meeting from "./components/Meeting/Meeting";
 import Apishka from "./components/Apishka/Apishka";
 
-
-
 function App() {
   return (
     <div>
-    <Router>
-        <Route exact path="/maps" component={MyPlacemark} />
+      <Router>
+        {/* <Route exact path="/maps" component={MyPlacemark} /> */}
         {/* <Route exact path="/maps2" component={MyPlacemark} /> */}
-        <Route exact path="/" component={HomePage}/>
+        <Route exact path="/" component={HomePage} />
         <Route exact path="/login" component={Login} />
         <Route exact path="/signup" component={SignUp} />
         <Route exact path="/logout" component={Logout} />
-        <Route exact path="/prodavito" component={Prodavito}/>
-        <Route exact path="/meeting" component={Meeting}/>
-        <Route exact path="/apishka" component={Apishka}/>
-    </Router>
+        <Route exact path="/prodavito" component={Prodavito} />
+        <Route exact path="/meeting" component={Meeting} />
+        <Route exact path="/apishka" component={PlacemarkDemo} />
+      </Router>
     </div>
   );
 }
--- client/src/components/Tinder/Tinder.jsx
+++ client/src/components/Tinder/Tinder.jsx
@@ -1,38 +1,36 @@
-import React from 'react';
+import React from "react";
 import TinderSearch from "../TinderSearch/TinderSearch";
 import TinderBox from "../TinderBox/TinderBox";
 
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import { CardActionArea } from '@mui/material';
-
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { CardActionArea } from "@mui/material";
 
 const Tinder = () => {
-    return (
-        <div>
-            <Card sx={{ maxWidth: 345 }}>
-                <CardActionArea>
-                    <CardMedia
-                        component="img"
-                        height="140"
-                        image="/static/images/cards/contemplative-reptile.jpg"
-                        alt="green iguana"
-                    />
-                    <CardContent>
-                        <Typography gutterBottom variant="h5" component="div">
-                            TinderCard
-                        </Typography>
-                        <Typography variant="body2" color="text.secondary">
-                            Тиндер для собак
-                        </Typography>
-                    </CardContent>
-                </CardActionArea>
-            </Card>
-
-        </div>
-    );
+  return (
+    <div>
+      <Card sx={{ maxWidth: 345 }}>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            height="140"
+            image="https://tinder.com/static/tinder.png"
+            alt="green iguana"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              TinderCard
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Тиндер для собак
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+    </div>
+  );
 };
 
 export default Tinder;
