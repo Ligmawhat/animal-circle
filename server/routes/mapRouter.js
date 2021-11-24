6b61b4f7aec6adf4db6c72a4afad780f54e86ce0
qqbit <pavlik.merkulov@gmail.com>
2021-11-24 18:04:50 +0300

    bug fix

--- server/routes/mapRouter.js
+++ server/routes/mapRouter.js
@@ -1,47 +1,27 @@
+const router = require("express").Router();
 
+const { Good, Category, User, Geotags } = require("../src/db/models");
 
-const router = require('express').Router()
+router.get("/", async (req, res) => {
+  const allTags = await Geotags.findAll();
+  console.log(allTags, "ALLTAGS");
+  res.json(allTags);
+});
 
-
-const { Good, Category, User, Geotags } = require('../src/db/models')
-
-
-router.get('/', async (req,res) => {
-  const allTags = await Geotags.findAll()
-  console.log(allTags, 'ALLTAGS')
-  res.json(allTags)
-})
-
-
-
-
-router.post('/new', async (req, res) => {
-  
-  if (!req.files || Object.keys(req.files).length === 0) {
-    return res.status(400).send('No files were uploaded.')
-  }
-  let sampleFile = req.files.file
-  sampleFile.name = Date.now()+'.jpg'
-  let uploadPath = `${process.env.PWD}/public/uploads/${sampleFile.name}`
-  sampleFile.mv(uploadPath, function (err) {
-    if (err) return res.status(500).send(err)
-  })
-  
-  console.log(req.session.userId, 'USERID MAP ROUTER')
-  console.log(req.session, 'REQ SESSION MAP ROUTER')
-
-  
-  const { cordsZero, cordsOne, title, desc} = req.body
-  console.log(cordsZero, cordsOne, title, desc, 'REQ BODY')
-  const newTags = await Geotags.create({
-    geotags_title: title,
-    latitude: cordsZero,
-    longitude: cordsOne,
-    description: desc,
-    url : sampleFile.name,
-    approved: false,
-    user_id : req.session.userId
+router.post("/new", async (req, res) => {
+  try {
+    if (!req.files || Object.keys(req.files).length === 0) {
+      return res.status(400).send("No files were uploaded.");
+    }
+    let sampleFile = req.files.file;
+    sampleFile.name = Date.now() + ".jpg";
+    let uploadPath = `${process.env.PWD}/public/uploads/${sampleFile.name}`;
+    sampleFile.mv(uploadPath, function (err) {
+      if (err) return res.status(500).send(err);
     });
+
+    // console.log(req.session.userId, 'USERID MAP ROUTER')
+    // console.log(req.session, 'REQ SESSION MAP ROUTER')
 
     const { cordsZero, cordsOne, title, desc } = req.body;
     const newTags = await Geotags.create({
